e023cf0b171eeb3c382eeb148aabb0bdc03f10e1
Eugene Ponizovsky <ponizovsky@gmail.com>
2016-08-21 20:54:50 +0300

    Rewritten generic example

--- examples/generic.pl
+++ examples/generic.pl
@@ -5,151 +5,102 @@ use warnings;
 
 use AnyEvent::RipeRedis::Cluster;
 use Data::Dumper;
-use Test::LeakTrace;
 
-printf ( "%x\n", AnyEvent::RipeRedis::Cluster::crc16( '123456789' ) );
-
-#no_leaks_ok {
-  my $redis = AnyEvent::RipeRedis::Cluster->new(
-    startup_nodes => [
-      { host => 'localhost', port => 7000 },
-      { host => 'localhost', port => 7001 },
-      { host => 'localhost', port => 7002 },
-    ],
-  default_slot     => 1,
-  allow_slaves     => 1,
+my $redis = AnyEvent::RipeRedis::Cluster->new(
+  startup_nodes => [
+    { host => 'localhost', port => 7000 },
+    { host => 'localhost', port => 7001 },
+  ],
   refresh_interval => 5,
-  lazy             => 1,
 
-    on_error => sub {
-      my $err = shift;
-      print Dumper( $err );
-    },
+  on_error => sub {
+    my $err = shift;
+    print Dumper($err);
+  },
 
-    on_node_connect => sub {
-      print "Connected\n";
-      print Dumper( \@_ );
-    },
+  on_node_connect => sub {
+    print "Connected\n";
+    print Dumper( \@_ );
+  },
 
-    on_node_disconnect => sub {
-      print "Disconnected\n";
-      print Dumper( \@_ );
-    },
+  on_node_disconnect => sub {
+    print "Disconnected\n";
+    print Dumper( \@_ );
+  },
 
-    on_node_error => sub {
-      print Dumper( \@_ );
-    },
-  );
+  on_node_error => sub {
+    print Dumper( \@_ );
+  },
+);
 
-  my $cv = AE::cv();
+my $cv = AE::cv;
+my $timer;
 
-  $cv->begin;
+$redis->get( '__last__',
+  sub {
+    my $last = shift || 0;
+    my $err  = shift;
 
-  $redis->set( 'foo', 'bar',
-    sub {
-      print Dumper( \@_ );
-      $cv->end;
-    },
-  );
+    if ( defined $err ) {
+      print Dumper($err);
+      $cv->send;
 
-  $cv->begin;
-
-  $redis->get( 'foo',
-    sub {
-      print Dumper( \@_ );
-      $cv->end;
+      return;
     }
-  );
 
-  $cv->begin;
+    $timer = AE::timer( 0, 0.1,
+      sub {
+        $redis->set( "foo$last", $last,
+          sub {
+            my $reply = shift;
+            my $err   = shift;
 
-  $redis->get( 'foo',
-    sub {
-      print Dumper( \@_ );
-      $cv->end;
-    }
-  );
+            if ( defined $err ) {
+              print Dumper($err);
+              return;
+            }
 
-  $cv->begin;
+            $redis->get( "foo$last",
+              sub {
+                my $reply = shift;
 
-  $redis->get( 'foo',
-    sub {
-      print Dumper( \@_ );
-      $cv->end;
-    }
-  );
+                if ( defined $err ) {
+                  print Dumper($err);
+                  return;
+                }
 
-#  $cv->begin;
-#
-#  $redis->eval_cached( 'return { KEYS[1], KEYS[2], ARGV[1], ARGV[2] }',
-#      2, '{key}1', '{key}2', 'first', 'second',
-#    sub {
-#      print Dumper( \@_ );
-#      $cv->end;
-#    }
-#  );
+                print "$reply\n";
+              }
+            );
 
-  $cv->begin;
+            $last++;
+          }
+        );
 
-  $redis->info(
-    sub {
-      print $_[0]->{redis_version} . "\n";
-      $cv->end;
-    }
-  );
+        $redis->set( '__last__', $last,
+          sub {
+            my $reply = shift;
+            my $err   = shift;
 
-  $cv->begin;
+            if ( defined $err ) {
+              print Dumper($err);
+              return;
+            }
+          }
+        );
+      }
+    );
+  }
+);
 
-  $redis->watch( 'foo831',
-    sub {
-      print Dumper( \@_ );
-    },
-  );
-  $redis->multi(
-    sub {
-      print 'multi -> ' . Dumper( \@_ );
-    }
-  );
-  $redis->get( 'foo',
-    sub {
-      print Dumper( \@_ );
-    }
-  );
-  #$redis->multi(
-  #  sub {
-  #    print Dumper( \@_ );
-  #  }
-  #);
-  $redis->get( 'foo',
-    sub {
-      print Dumper( \@_ );
-    }
-  );
-  $redis->exec(
-    { on_reply => sub {
-        print Dumper( \@_ );
-        $cv->end;
-      },
-      on_node_error => sub {
-        print Dumper( \@_ );
-      },
-    }
-  );
+my $on_signal = sub {
+  print "Stopped\n";
+  $cv->send;
+};
 
-  $cv->begin;
+my $int_w  = AE::signal( INT  => $on_signal );
+my $term_w = AE::signal( TERM => $on_signal );
 
-  $redis->get( 'foo',
-    { on_reply => sub {
-        print Dumper( \@_ );
-        $cv->end;
-      },
-      on_node_error => sub {
-        print Dumper( \@_ );
-      },
-    }
-  );
+$cv->recv;
 
-  $cv->recv();
-
-  undef $redis;
-#} 'leaks';
+$redis->disconnect;
